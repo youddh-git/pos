f892f14a603eac3a5d469a6637d7a32dfb4a80b6
Parakramsinh Parmar <im.pk.parmar@gmail.com>
2020-07-28 11:26:51 +0530

    test

---
+++ src/main/java/com/hospisoft/store/Test.java
@@ -0,0 +1,25 @@
+package com.hospisoft.store;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		// create a list of strings 
+        List<String> names = Arrays.asList("Geek","GeeksQuiz","g1","QA","Geek2"); 
+  
+        // declare the predicate type as string and use 
+        // lambda expression to create object 
+        Predicate<String> p = (s)->s.startsWith("G"); 
+  
+        // Iterate through the list 
+        for (String st:names){ 
+            // call the test method 
+            if (p.test(st)) 
+                System.out.println(st); 
+        } 
+	}
+}
---
+++ src/main/java/com/hospisoft/store/controller/CorsFilter.java
@@ -0,0 +1,56 @@
+package com.hospisoft.store.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+	
+	@Value("${app.api.settings.cross-origin.urls}")
+	private String cross_origin_urls;
+	
+	
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+    	System.out.println("CorsFilter.doFilter()");
+    	System.out.println(cross_origin_urls);
+    	
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", cross_origin_urls);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "false");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");        
+        if (request.getMethod().equals(HttpMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+        }else {
+            chain.doFilter(req, res);
+        }
+    }
+    
+    public void init(FilterConfig filterConfig) {
+    }
+
+    public void destroy() {
+    }
+}
